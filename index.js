8d1ddb79e39bfd2f5ea5cee3411c5799d327dc2e
Dreezy305 <bankoleidris@gmail.com>
2021-07-28 10:35:57 +0000

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.listen(5000, () => {
+  console.log(`Server is running on port 5000.`);
+});
+
 const books = [
   { title: "Harry Potter", id: 1 },
   { title: "Twilight", id: 2 },
@@ -11,3 +15,10 @@ const books = [
 ];
 
 // READ REQUEST HANDLERS
+app.get("/", (req, res) => {
+  res.send("This is my CRUD api");
+});
+
+app.get("/api/books", (req, res) => {
+  res.send(books);
+});
