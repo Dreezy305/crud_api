f833eb049047e11502b16b0f4ab2f271c8a49f1a
Dreezy305 <bankoleidris@gmail.com>
2021-07-28 10:32:15 +0000

    first commit

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const joi = require("joi");
 const app = express();
 
 app.use(express.json());
+
+const books = [
+  { title: "Harry Potter", id: 1 },
+  { title: "Twilight", id: 2 },
+  { title: "Lorien Legacies", id: 3 },
+];
+
+// READ REQUEST HANDLERS
